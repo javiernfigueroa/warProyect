5b27969e0882b0bf5829b1d46b37468a353a123a
Javier Figueroa <109968115+javiernfigueroa@users.noreply.github.com>
2023-04-24 14:10:33 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+
+const fetchDeck = function() {
+    fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
+        .then(res => res.json())
+        .then(data => console.log(data))
+}
+
+
+document.getElementById("new-deck").addEventListener("click", fetchDeck)
